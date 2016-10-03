e39e742a0521f77cbbeb5961f495abcb4b912702
ZioGene <giuseppe.zappulla1987@gmail.com>
2016-10-03 12:24:43 +0200

    Update conf.solution1.js

--- e2e/conf.solution1.js
+++ e2e/conf.solution1.js
@@ -3,7 +3,7 @@
 exports.config = {
     framework: 'jasmine',
 
-    seleniumServerJar: process.env[(process.platform == 'win32') ? 'APPDATA' : 'NODE_PATH'] + '/npm/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.1.jar',
+    seleniumServerJar: '../../node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar',
     seleniumPort: null,
 
     suites: {
