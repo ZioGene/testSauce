b6de8036fb693d796a63b092a0cfd7ec552145c3
ZioGene <giuseppe.zappulla1987@gmail.com>
2016-10-04 17:56:17 +0200

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 // conf.js
-var username = "Cyborg",
-    accessKey = "83006b9b-f79e-4f60-8866-0c9ed66eecac";
+var username = "YourUser",
+    accessKey = "YourAccessKey";
 exports.config = {
     framework: 'jasmine',
 
